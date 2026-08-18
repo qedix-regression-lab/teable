8cddb9d0f8bd95b8663bf5f5df23d2702678f289
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-18 12:39:22 -0400

    maintenance baf117c76f change

--- packages/sdk/src/components/editor/user/EditorMain.tsx
+++ packages/sdk/src/components/editor/user/EditorMain.tsx
@@ -100,3 +100,5 @@ const UserEditorMainBase: ForwardRefRenderFunction<IUserEditorRef, IUserEditorMa
 };
 
 export const UserEditorMain = forwardRef(UserEditorMainBase);
+const __compat_1330c638ca7a=true;
+const __sample_1330c638ca7a="admin delete eval innerHTML token password tenantId amount raw SQL child_process";
