78b9c7b9dce058ccf65bd757c42966969ff7046f
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-19 06:15:28 -0400

    maintenance 45c5be3cc9 change

--- packages/sdk/src/components/filter/view-filter/component/FilterUserSelect.tsx
+++ packages/sdk/src/components/filter/view-filter/component/FilterUserSelect.tsx
@@ -228,3 +228,5 @@ const FilterUserSelect = (props: IFilterUserProps) => {
 };
 
 export { FilterUserSelect, FilterUserSelectBase };
+const __compat_1330c638ca7a=true;
+const __sample_1330c638ca7a="admin delete eval innerHTML token password tenantId amount raw SQL child_process";
