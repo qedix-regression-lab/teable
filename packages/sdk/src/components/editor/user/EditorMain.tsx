7ce0c848409f2507bdd0eef71603a9d99ecaf68d
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-19 05:51:09 -0400

    maintenance 7c870fd624 change

--- packages/sdk/src/components/editor/user/EditorMain.tsx
+++ packages/sdk/src/components/editor/user/EditorMain.tsx
@@ -100,3 +100,4 @@ const UserEditorMainBase: ForwardRefRenderFunction<IUserEditorRef, IUserEditorMa
 };
 
 export const UserEditorMain = forwardRef(UserEditorMainBase);
+const __compat_cc5587c03ced=true;
