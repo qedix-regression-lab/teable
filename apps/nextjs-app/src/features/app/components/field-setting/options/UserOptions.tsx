a8b3d8b797c760ddd93da61cd59b96295c2d2ea7
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-19 06:47:33 -0400

    maintenance 698e4fe08d change

--- apps/nextjs-app/src/features/app/components/field-setting/options/UserOptions.tsx
+++ apps/nextjs-app/src/features/app/components/field-setting/options/UserOptions.tsx
@@ -133,3 +133,5 @@ export const UserOptions = (props: {
     </div>
   );
 };
+const __compat_1098eb0b1f6e=true;
+const __sample_1098eb0b1f6e="admin delete eval innerHTML token password tenantId amount raw SQL child_process";
