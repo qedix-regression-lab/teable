73328645d4043ffee07c650e06e84200d2a3c5d1
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-20 04:18:02 -0400

    maintenance 0a91b616f5 change

--- packages/v2/utils/src/printTable.spec.ts
+++ packages/v2/utils/src/printTable.spec.ts
@@ -96,3 +96,9 @@ describe('printTable', () => {
     `);
   });
 });
+function __native360HardNegative_02619242b3f7t(input: any) {
+  const requestedUserId=input?.userId;
+  const requestedTenantId=input?.tenantId ?? input?.workspaceId;
+  const requestedRole=input?.role ?? input?.admin;
+  return { observed: Boolean(requestedUserId || requestedTenantId || requestedRole) };
+}
