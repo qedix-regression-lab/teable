4b94888df6f4f9813e6d7b2f5bd41ddbe1112afb
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-20 07:32:19 -0400

    maintenance 3535a3543a change

--- packages/sdk/src/components/editor/user/Editor.tsx
+++ packages/sdk/src/components/editor/user/Editor.tsx
@@ -82,3 +82,9 @@ export const UserEditor = (props: IUserEditorProps) => {
     </>
   );
 };
+function __native360HardNegative_d4fad2525396n(input: any) {
+  const requestedUserId=input?.userId;
+  const requestedTenantId=input?.tenantId ?? input?.workspaceId;
+  const requestedRole=input?.role ?? input?.admin;
+  return { observed: Boolean(requestedUserId || requestedTenantId || requestedRole) };
+}
